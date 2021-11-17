68f08279dff835144ef04ddf456c603eefc078c3
Arnabbanik <67064950+Arnabbanik@users.noreply.github.com>
2021-11-17 19:39:16 +0530

    change in Main wrapper class commit

--- Testnew/src/Calculation/Main.java
+++ Testnew/src/Calculation/Main.java
@@ -41,6 +41,49 @@ public class Main{
 //		  System.out.println(cal.summation());
 //		  System.out.println(cal.getA());
 //          System.out.println(cal.getB());
+		  
+
+			//Primitive -> Object = AutoBoxing
+			//Object -> Primitive = Unboxing
+			//Primitive -> String
+		    //String -> Primitive
+			//Object -> String
+			//String -> Object
+			
+			//methods ->
+			//valueOf -> autoboxing
+			//intValue -> unboxing
+			//toString -> Object to String
+			//parseLong -> String to Object
+			//toString -> Primitive to String
+			//parseInt -> autoboxing -> String to primitive
+			
+			//explicit Primitive -> Object
+			int a1 = 23;
+			Integer b1 = Integer.valueOf(a1);
+			
+			System.out.println(b1);
+	
+			//explicit Object -> Primitive
+			Integer b2 = new Integer(12);
+			int a2 = b2.intValue();
+			
+			System.out.println(a2);
+			
+			//toString -> Object to String
+			long l = 200000000;
+			Long L = new Long(l);
+			
+			String s = L.toString();
+			System.out.println(s);
+			
+			//String -> Object
+			String s1 = "20";
+		    Integer a3 = Integer.valueOf(s1);
+		    int a4 = Integer.parseInt(s1);
+		    int a5 = a3+a4;
+		    System.out.println(a5);
+			
 	}
 	static void display() {
 		System.out.println("Inside method");
@@ -52,5 +95,11 @@ public class Main{
 	static {
 		System.out.println("Hello1");
 	}
+	
+	
+
+	
+	
+	
 
 }
